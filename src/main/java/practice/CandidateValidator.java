527a247dadcdbd7a0271fe34b94b08a849b2ef1c
Kamil Jan Mularski <kammul@outlook.com>
2025-10-02 23:46:14 +0200

    Implement validation and stream utilities
    Added CandidateValidator with custom validation logic. Implemented various StreamPractice methods for processing lists, including finding minimum even numbers, calculating averages, filtering people by age and sex, extracting cat names, and validating candidates.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,30 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+/**
+ * The requirements are: person should be older than 35 years, should be allowed to vote,
+ * have nationality - 'Ukrainian'
+ * and live in Ukraine for 10 years. For the last requirement use field periodsInUkr,
+ * which has following view: "2002-2015"
+ */
+public class CandidateValidator implements Predicate<Candidate> {
+
+    private static final int AGE_REQUIREMENT = 35;
+    private static final int LIVING_IN_UKR_REQUIREMENT = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        boolean isUkrainian = candidate.getNationality().equals("Ukrainian");
+        boolean isOlderThan35 = candidate.getAge() >= AGE_REQUIREMENT;
+        boolean allowedToVote = candidate.isAllowedToVote();
+        String[] split = candidate.getPeriodsInUkr().split("-");
+        int startYear = Integer.parseInt(split[0]);
+        int endYear = Integer.parseInt(split[1]);
+        int inUkr = endYear - startYear;
+        boolean living10YearsInUkr = inUkr >= LIVING_IN_UKR_REQUIREMENT;
+        return isOlderThan35 && allowedToVote && isUkrainian && living10YearsInUkr;
+    }
 }
